adc276b51ac903447c74829d00c8c130fe5467ab
YS <yoonsam86@gmail.com>
2022-03-20 16:20:08 +0900

    flow exceptionhandling - 2. everything is caught

---
+++ app/src/main/java/com/ys/coroutinestudy/playground/flow/exceptionhandling/2-everything-is-caught.kt
@@ -0,0 +1,27 @@
+package com.ys.coroutinestudy.playground.flow.exceptionhandling
+
+import com.ys.coroutinestudy.util.log
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
+
+private fun simple(): Flow<String> =
+	flow {
+		for (i in 1..3) {
+			println("Emitting $i")
+			emit(i) // emit next value
+		}
+	}.map { value ->
+		check(value <= 1) { "Crashed on $value" }
+		"string $value"
+	}
+
+fun main() = runBlocking {
+	try {
+		simple().collect { value -> log(value) }
+	} catch (e: Exception) {
+		println("Caught $e")
+	}
+}
