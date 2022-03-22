9db5923c77ff30a63ab205619be41565662e61fa
YS <yoonsam86@gmail.com>
2022-03-23 08:29:58 +0900

    completion - 1. imperative finally block

---
+++ app/src/main/java/com/ys/coroutinestudy/playground/flow/completion/1-imperative-finally-block.kt
@@ -0,0 +1,17 @@
+package com.ys.coroutinestudy.playground.flow.completion
+
+import com.ys.coroutinestudy.util.log
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.runBlocking
+
+private fun simple(): Flow<Int> = (1..3).asFlow()
+
+fun main() = runBlocking {
+	try {
+		simple().collect { value -> log(value) }
+	} finally {
+		println("Done")
+	}
+}
