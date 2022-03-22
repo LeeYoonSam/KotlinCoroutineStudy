ed5df6ae002700f62be51ad0016257f2e216f97e
YS <yoonsam86@gmail.com>
2022-03-23 08:47:42 +0900

    completion - 2. declarative handling

--- app/src/main/java/com/ys/coroutinestudy/playground/flow/completion/2-declarative-handling.kt
+++ app/src/main/java/com/ys/coroutinestudy/playground/flow/completion/2-declarative-handling.kt
@@ -4,14 +4,13 @@ import com.ys.coroutinestudy.util.log
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.runBlocking
 
 private fun simple(): Flow<Int> = (1..3).asFlow()
 
 fun main() = runBlocking {
-	try {
-		simple().collect { value -> log(value) }
-	} finally {
-		log("Done")
-	}
+	simple()
+		.onCompletion { log("Done") }
+		.collect { value -> log(value) }
 }
